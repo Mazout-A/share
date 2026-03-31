9f3996359a6c215d8ee2f4061606c43aa6fa7b72
Billal Boudlal <Billalboudlal2210@outlook.fr>
2026-03-31 15:17:02 +0200

    Toutes les actions du controller Users on etait mis :
    function beforeFilter est la pour permet au rpospect de s'incrire ou de voir les activité sans qu'il soit bloqué.
    
    function index permet de voir tous les users.
    
    function view permet de voir le profil user.
    
    fucntion add permet de d'ajouter un user.
    
    function edit permet de modfifier un profil.
    
    fucntion delete permet de supprimer un comtpe.
    
    function login permet de se connecter.
    
    function logout permet de se deconnecter.

---
+++ src/Controller/UsersController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Controller;
+
+use Cake\Event\EventInterface;
+
+class UsersController extends AppController
+{
+    // La fonction sert permet au prospect de s'inscrire ou de voir les activité sélectionnés sans etre bloqué
+    public function beforeFilter(\Cake\Event\EventInterface $e) : void
+    {
+        parent::beforeFilter($e);
+        $this->Authentication->allowUnauthenticated(
+        ['login', 'add']);
+    }
+
+    // fonction index pour voir tous les users.
+    public function index()
+    {
+        $user = $this->User->find();
+        $users =$this->paginate($user);
+
+        $this->set(compact('users'));
+    }
+
+    // fonction view pour voir les profil user.
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, contain: []);
+        $this->set(compact('user'));
+    }
+
+    // Fonction add pour ajouter un nouveau compte
+    public function add()
+    {
+        $user = $this->Users->newEmptiEntity();
+        if ($this->request->is('post')) {
+            $user = $this->Users->patchEntity(
+                $user, $this->request->getdata());
+                if ($this->Users->save($user)) {
+                    $this->Flash->success('Votre compte a bien etait créer');
+
+                    return $this->redirect(['action' => 'view']);
+                }
+                $this->Flash->error('Votre compte n\'a pas était creer');
+        }
+        $this->set(compact('user'));
+    }
+
+    // fonction qui permet de modifier les données d'un compte
+    public function edit($id = null)
+    {
+        $user = $this->Isers->get($id, contain: []);
+        if($this->request->is(['patch', 'post', 'put']))
+            {
+                $user = $this->Users->patchEntity($user,
+                $this->request->getDate());
+                if($this->User->save($user)) {
+                    $this->Flash->success('Les modification sont sauvegarder');
+
+                    return $this->redirect(['action' => 'view']);
+                }
+                $this->Flash->error('Les modification n\'ont etait pris en compte');
+            }
+            $this->set(compact('user'));
+    }
+
+    // fonction qui permet de delete un compte
+    public function delete($id = null)
+    {
+        $this->request->allowMethod(['post', 'delete']);
+        $user = $this->users->get($id);
+        if ($this->Users->delete($user)){
+            $this->Flash->success('Votre compte a était supprimer');
+        } else{
+            $this->Flash->error('Votre compte n\'pas pu etre supprimé');
+        }
+        return $this->redirect(['action' => 'add']);
+    }
+
+    // function qui permet de se connecter
+    public function login()
+    {
+        $result = $this->Authentication->getResult();
+        if($result && $result->isValid())
+            {
+                $target = $this->Authentication->getLoginRedirect() ?? [
+                    'controller' => 'a',
+                    'action' => 'index'
+                ];
+                return $this->redirect($target);
+            }
+            if ($this->request->is('post'))
+                {
+                    $this->Flash->error('prénom ou mot de passe incorrect');
+                }
+    }
+
+    public function logout()
+    {
+        $this->Authentication->logout();
+        return $this->redirect([
+            'controller' => 'Users',
+            'action' => 'login'
+        ]);
+    }
+}
+
